243b1daf91ae2e83ee60291085825dfd21ef97ed
pekemee <riyoniriverplus2@yahoo.co.jp>
2020-03-16 16:36:19 +0900

    my first commit

---
+++ text.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+#include <gconio.h>
+#include <string.h>
+#include <math.h>
+#include <ctype.h>
+#define COUNT 100
+void sum_it(int value);
+
+int  main(int argc,char *argv[]){
+  int num;
+  #line 1000 "text.c"
+  #error Check line.no & file name.
+  return 0;
+}
